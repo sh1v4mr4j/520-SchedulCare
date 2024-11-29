f529fcccfa2e186058605793049b8135f8d4f2f9
sniggy10 <snigdhaansu@gmail.com>
2024-11-28 19:09:09 -0500

    Adding backend code for payments feature

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -4,6 +4,14 @@ import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 // Renders errors or successful transactions on the screen.
 const Message = ({ content }) => <p>{content}</p>;
 
+// TO DO : MOve out from here
+const API_BASE_URL = "http://127.0.0.1:8000/";
+const ENDPOINTS = {
+  createOrder: `${API_BASE_URL}payments/orders`,
+  capturePayment : (order_id) => `${API_BASE_URL}payments/orders/${order_id}/capture`,
+};
+
+
 export const PaymentForm = () => {
   const initialOptions = {
     "client-id": "test",
@@ -30,7 +38,8 @@ export const PaymentForm = () => {
           }}
           createOrder={async () => {
             try {
-              const response = await fetch("/api/orders", {
+              const url = ENDPOINTS.createOrder;
+              const response = await fetch(url, {
                 method: "POST",
                 headers: {
                   "Content-Type": "application/json",
@@ -39,17 +48,20 @@ export const PaymentForm = () => {
                   cart: [
                     {
                       id: "YOUR_PRODUCT_ID",
-                      quantity: "YOUR_PRODUCT_QUANTITY",
+                      name: "YOUR_PRODUCT_ID",
+                      price: 100.0,
+                      quantity: 1,
                     },
                   ],
                 }),
               });
 
-              const orderData = await response.json();
+              const responseOrderData= await response.json();
+              const orderData = JSON.parse(responseOrderData);
 
-              if (orderData.id) {
+              if (orderData && orderData.id) {
                 return orderData.id;
-              } else {
+            } else {
                 const errorDetail = orderData?.details?.[0];
                 const errorMessage = errorDetail
                   ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
@@ -64,8 +76,9 @@ export const PaymentForm = () => {
           }}
           onApprove={async (data, actions) => {
             try {
+              const url = ENDPOINTS.capturePayment(data?.orderID);
               const response = await fetch(
-                `/api/orders/${data.orderID}/capture`,
+                url,
                 {
                   method: "POST",
                   headers: {
@@ -74,8 +87,10 @@ export const PaymentForm = () => {
                 }
               );
 
-              const orderData = await response.json();
+              const responseOrderData = await response.json();
+              const orderData = JSON.parse(responseOrderData);
 
+              console.log("Order Data Response:", orderData);
               const errorDetail = orderData?.details?.[0];
 
               if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
