addb5dd5a316afb6ebd7f9d83fcaf6d75c9bbcc6
sniggy10 <snigdhaansu@gmail.com>
2024-12-05 05:36:48 -0500

    Adding appointment router

--- server/app/main.py
+++ server/app/main.py
@@ -1,14 +1,16 @@
 from dotenv import load_dotenv
 import sys
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
 from app.routers import patient_router
 from app.routers import doctor_router
 from app.shared.response import Response
 from app.routers import payment_router
 from app.routers import email_router
 from app.routers import chat_router
-
+from app.routers import appointment_router
 
 def load_environment():
     """
@@ -43,8 +45,9 @@ async def health_check():
     return Response(status_code=200, body="I'm alive")
 
 # Include the routers
-app.include_router(patient_router.app, prefix="/patients", tags=["patients"])
+app.include_router(patient_router.app, prefix="/patients", tags=["Patients"])
 app.include_router(doctor_router.app, prefix="/doctors", tags=["Doctor"])
 app.include_router(payment_router.app, prefix="/payments", tags=["Payments"])
 app.include_router(email_router.app, prefix="/email", tags=["EmailNotification"])
-app.include_router(chat_router.router, prefix="/chat", tags=["chat"])
+app.include_router(chat_router.app, prefix="/chat", tags=["Chat"])
+app.include_router(appointment_router.app, prefix="/appointment", tags=["Appointment"])
